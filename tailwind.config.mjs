ae3cd35592bfdee0a8da6aff4f9dbb630e76deb9
Aaron Bassett <aaronbassett@gmail.com>
2024-05-21 12:37:06 -0400

    Block out initial docs layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@ import polkadotBrandColors from "./src/config/polkadot-brand-colors"
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["selector", '[data-mode="dark"]'],
   content: [
     "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./node_modules/flowbite/**/*.js",
@@ -18,5 +19,5 @@ export default {
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("flowbite/plugin"), require("flowbite-typography")],
 }
